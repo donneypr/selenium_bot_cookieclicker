3a9ad0d20f1ff710e217fd17602ba1f6c62b3c34
John Donato Prabahar <122427011+donneypr@users.noreply.github.com>
2024-09-23 13:08:20 -0400

    Uploaded basic chrome opening

---
+++ main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+service = Service(executable_path="/Users/donatoprabahar/Desktop/coding_projects/selenium-project/chromedriver")
+driver = webdriver.Chrome(service = service)
+
+driver.get("https://google.com")
+
+time.sleep(10)
+driver.quit()
